7e86176f191a2d75c53eeeba3e7a2d8640dcd21a
Behzodjon <behzodjontuit1998@gmail.com>
2023-01-20 16:46:31 +0500

    fix: bugs

--- api/models/index.js
+++ api/models/index.js
@@ -1,24 +1,31 @@
 //importing modules
-const {Sequelize, DataTypes} = require('sequelize')
+const { Sequelize, DataTypes } = require('sequelize');
 
 //Database connection with dialect of postgres specifying the database we are using
 //port for my database is 5433
 //database name is discover
-const sequelize = new Sequelize(`postgres://postgres:root@localhost:5432/auth_db`, {dialect: "postgres"})
+// const sequelize = new Sequelize(`postgres://postgres:root@localhost:5432/auth_db`, {dialect: "postgres"})
+const sequelize = new Sequelize(
+  `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:5432/${process.env.DB_NAME}`,
+  { dialect: 'postgres' }
+);
 
 //checking if connection is done
-    sequelize.authenticate().then(() => {
-        console.log(`Database connected to discover`)
-    }).catch((err) => {
-        console.log(err)
-    })
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log(`Database connected to discover`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-    const db = {}
-    db.Sequelize = Sequelize
-    db.sequelize = sequelize
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
 
 //connecting to model
-db.users = require('./user') (sequelize, DataTypes)
+db.users = require('./user')(sequelize, DataTypes);
 
 //exporting the module
-module.exports = db
+module.exports = db;
